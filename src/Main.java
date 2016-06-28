98b8d49146d7d05746c8128524134e9dcd18d49e
ak11234 <akpc556@gmail.com>
2016-06-28 13:02:45 -0500

    First

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Adam on 6/28/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Print.printone();
+        Print.printtwo();
+        Print.printsumtwo();
+        Print.printaveragetwo();
+        Print.printsumthree();
+        Print.printthreeaverage();
+        */
+
+    }
+}
---
+++ src/Print.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+/**
+ * Created by Adam on 6/28/2016.
+ */
+public class Print {
+    public static double getint(String description){
+        Scanner scan = new Scanner(System.in);
+        double num;
+
+        System.out.print(description);
+        num = scan.nextDouble();
+        return num;
+    }
+    public static void printone(){
+        Scanner scan = new Scanner(System.in);
+        double num;
+
+        System.out.print("Enter a number: ");
+
+        num = scan.nextDouble();
+
+        System.out.println("You entered: " + num);
+    }
+    public static void printtwo(){
+        Scanner scan = new Scanner(System.in);
+        double num1;
+        double num2;
+
+        System.out.println("Enter a number: ");
+        num1 = scan.nextDouble();
+        System.out.println("Enter another number: ");
+        num2 = scan.nextDouble();
+        System.out.println("You entered: " + num1 + " and " +num2);
+    }
+    public static void printsumtwo(){
+        Scanner scan = new Scanner(System.in);
+        double num1;
+        double num2;
+
+        System.out.println("Enter a number: ");
+        num1 = scan.nextDouble();
+        System.out.println("Enter another number: ");
+        num2 = scan.nextDouble();
+        double sum = num1+num2;
+        System.out.println("Your sum is " + sum);
+    }
+    public static void printaveragetwo() {
+        double num1, num2;
+        num1 = getint("First number: ");
+        num2 = getint("Second number: ");
+        double dec1, dec2;
+        dec1 = num1 + 0.0;
+        dec2 = num2 + 0.0;
+        double average = ((num1 + num2) / 2.0);
+        System.out.println(average);
+    }
+    public static void printsumthree(){
+        double num1, num2, num3;
+        num1=getint("First number: ");
+        num2=getint("Second number: ");
+        num3=getint("Third number: ");
+        System.out.println(num1+num2+num3);
+    }
+    public static void printthreeaverage(){
+        double num1,num2,num3;
+        num1=getint("First number: ");
+        num2=getint("Second number: ");
+        num3=getint("Third number: ");
+        double average = ((num1 + num2 + num3) / 3.0);
+        System.out.println(average);
+    }
+}
